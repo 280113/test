b4a87f0019230ba2855c47d27c8d68614aa4f549
Federico Quarta <s280113@studenti.polito.it>
2020-11-13 20:21:05 +0100

    test

--- test/lecture.js
+++ test/lecture.js
@@ -81,12 +81,6 @@ describe("Lecture test", async () => {
                 .expect(200, {message: "Booking created."});
         });
 
-        it("should receive an email", async() => {
-            const inbox = (await mailslurp.getInboxes())[0];
-            mailslurp.emptyInbox(inbox.id); 
-            const email = await mailslurp.waitForLatestEmail(inbox.id);
-            expect(email.body).to.equal("test");
-        });
     });
 });
   
